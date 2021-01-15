67f8f25f801032c6b69d8c5ced88e02a846e5d2d
nikjag <nikitagolubew@yandex.ru>
2021-01-15 16:58:09 +0300

    Контроллеры
    Изменён метод StockController.findAll.

--- src/main/java/ru/study/shop/adapters/controllers/StockController.java
+++ src/main/java/ru/study/shop/adapters/controllers/StockController.java
@@ -1,15 +1,19 @@
 package ru.study.shop.adapters.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.study.shop.entities.Stock;
 import ru.study.shop.services.interfaces.StockService;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/shop/stock")
+@RequestMapping("/shop/stocks")
 public class StockController {
 
     private StockService stockService;
@@ -19,7 +23,34 @@ public class StockController {
     }
 
     @GetMapping()
-    public List<Stock> findAll() {
-        return stockService.findAll();
+    public ResponseEntity<List<Stock>> findAll(
+        @RequestParam(value = "size", required = false) String size,
+        @RequestParam(value = "productId", required = false) Long productId) {
+
+        boolean sizeIsNull = Objects.isNull(size);
+        boolean productIdIsNull = Objects.isNull(productId);
+
+        if (sizeIsNull && productIdIsNull) {
+            return ResponseEntity.ok(stockService.findAll());
+        }
+
+        if (sizeIsNull) {
+            return ResponseEntity.ok(
+                stockService.findAll().stream()
+                    .filter(stock -> stock.getProduct().getId().equals(productId))
+                    .collect(Collectors.toList()));
+        }
+
+        if (productIdIsNull) {
+            return ResponseEntity.ok(
+                stockService.findAll().stream()
+                    .filter(stock -> stock.getSize().equals(size))
+                    .collect(Collectors.toList()));
+        }
+
+        return ResponseEntity.ok(
+            stockService.findAll().stream()
+                .filter(stock -> stock.getSize().equals(size) && stock.getProduct().getId().equals(productId))
+                .collect(Collectors.toList()));
     }
 }

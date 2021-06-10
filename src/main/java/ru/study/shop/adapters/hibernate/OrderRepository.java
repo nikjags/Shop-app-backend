be2b74d0f2e873b3f2bd71c040885df117bc7cad
nikjag <nikitagolubew@yandex.ru>
2021-06-10 20:50:32 +0300

    added new methods in OrderRepository for date and time searching

--- src/main/java/ru/study/shop/adapters/hibernate/OrderRepository.java
+++ src/main/java/ru/study/shop/adapters/hibernate/OrderRepository.java
@@ -1,7 +1,38 @@
 package ru.study.shop.adapters.hibernate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.study.shop.entities.Order;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    LocalDateTime MIN_ORDER_DATE_TIME = LocalDateTime.MIN;
+    LocalDateTime MAX_ORDER_DATE_TIME = LocalDateTime.MAX;
+
+    @Query(value =
+            "SELECT * " +
+            "FROM orders " +
+            "WHERE ordered_time >= ?1 ",
+        nativeQuery = true
+    )
+    List<Order> findAllFromDate(@Param("fromDate") LocalDateTime fromDate);
+
+    @Query(value =
+        "SELECT * " +
+            "FROM orders " +
+            "WHERE ordered_time <= ?1 ",
+        nativeQuery = true
+    )
+    List<Order> findAllToDate(@Param("fromDate") LocalDateTime toDate);
+
+    @Query(value =
+            "SELECT * " +
+            "FROM orders " +
+            "WHERE ordered_time BETWEEN ?1 AND ?2 ",
+        nativeQuery = true
+    )
+    List<Order> findAllFromDateToDate(@Param("fromDate") LocalDateTime fromDate, @Param("toDate") LocalDateTime toDate);
 }

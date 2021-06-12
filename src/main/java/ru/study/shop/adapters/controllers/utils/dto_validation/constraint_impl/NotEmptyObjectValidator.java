8b6e981e79f849cfd9a3089babbb305301169275
nikjag <nikitagolubew@yandex.ru>
2021-06-12 19:44:29 +0300

    i do love fixes in NotEmptyObjectvalidator

--- src/main/java/ru/study/shop/adapters/controllers/utils/dto_validation/constraint_impl/NotEmptyObjectValidator.java
+++ src/main/java/ru/study/shop/adapters/controllers/utils/dto_validation/constraint_impl/NotEmptyObjectValidator.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class NotEmptyObjectValidator implements ConstraintValidator<NotEmptyObject, Object> {
     @Override
@@ -27,8 +28,8 @@ public class NotEmptyObjectValidator implements ConstraintValidator<NotEmptyObje
                 PropertyDescriptor propertyDescriptor = new PropertyDescriptor(fieldName, value.getClass());
                 Object propertyValue = propertyDescriptor.getReadMethod().invoke(value);
 
-                if (isNull(propertyValue)) {
-                    return false;
+                if (nonNull(propertyValue)) {
+                    return true;
                 }
             } catch (IntrospectionException | IllegalAccessException | InvocationTargetException e) {
                 e.printStackTrace();
@@ -36,6 +37,6 @@ public class NotEmptyObjectValidator implements ConstraintValidator<NotEmptyObje
             }
         }
 
-        return true;
+        return false;
     }
 }

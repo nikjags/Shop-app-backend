2537953ae9e2e2a7d572bec6f9865477255b7936
nikjag <nikitagolubew@yandex.ru>
2021-06-13 19:29:40 +0300

    added new methods in StockRepository

--- src/main/java/ru/study/shop/adapters/hibernate/StockRepository.java
+++ src/main/java/ru/study/shop/adapters/hibernate/StockRepository.java
@@ -1,11 +1,32 @@
 package ru.study.shop.adapters.hibernate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ru.study.shop.entities.Product;
 import ru.study.shop.entities.Stock;
 import ru.study.shop.entities.StockId;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, StockId> {
     Optional<Stock> findByProductIdAndSize(Long productId, String size);
+
+    List<Stock> findByProduct(Product product);
+
+    List<Stock> findByProductId(Long productId);
+
+    List<Stock> findBySize(String size);
+
+    @Query(
+        "SELECT s " +
+        "FROM Stock s " +
+        "WHERE s.quantity = 0")
+    List<Stock> findEmptyStocks();
+
+    @Query(
+        "SELECT s " +
+        "FROM Stock s " +
+        "WHERE s.quantity <> 0")
+    List<Stock> findNonEmptyStocks();
 }
